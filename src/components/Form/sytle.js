d2b7523974651660b8d33464763b0e3aa57c69aa
barbosadahora <103425559+barbosadahora@users.noreply.github.com>
2026-06-16 23:44:07 -0300

    Refactor styles in Form component

--- src/components/Form/sytle.js
+++ src/components/Form/sytle.js
@@ -1,60 +1,21 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    formContext: {
-        width: "100%",
-        height: "70%",
-        bottom:0,
-        backgroundColor:"#e5ebf1",
-        alignItems:"center",
-        marginTop:5,
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30,
-        borderBottomLeftRadius:30,
-        borderBottomRightRadius:30
+export const styles = StyleSheet.create({
+    container: {
+        alignItems:"center"
     },
-
-    form:{
-        width:"100%",
-        height: "auto",
-        marginTop: 30,
-        padding: 10
-
+    formLabel: {
+        color: "#000000",
+        fontSize: 18,
+        paddingLeft: 20,
+        fontWeight:'bold'
     },
-
-    formLabel:{
-        color:"#000000",
-        fontSize:18,
+    formValor:{
+        color:"#1967d2",
+        fontSize:20,
+        fontWeight:'bold',
         paddingLeft: 20
-    },
-
-    input:{
-        width:"90%",
-        borderRadius:50,
-        backgroundColor: "#f6f6f6",
-        height:40,
-        margin:12,
-        paddingLeft:10
-    },
-
-    textButton:{
-        fontSize: 20,
-        color:"#ffffff"
-    },
-
-    button:{
-        borderRadius:50,
-        alignItems:"center",
-        justifyContent:"center",
-        width:"90%",
-        backgroundColor:"#1967d2",
-        paddingTop:14,
-        paddingBottom:14,
-        marginLeft:12,
-        margin:30
     }
-
-
 });
 
 export default styles
