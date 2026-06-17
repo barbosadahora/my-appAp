580696f2662be195e6f7e34627163c5c9203a1be
barbosadahora <103425559+barbosadahora@users.noreply.github.com>
2026-06-16 23:44:52 -0300

    Update index.js

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { styles } from "./style";
+import React, { useState } from "react";
+import { TextInput, Text, TouchableOpacity, View } from "react-native";
+import { Result } from "../Result"; 
+import styles from "./style"; 
+import getConexaoapi from "../../services/conexaoapi";
 
-export function Result(props) {
+export default function Form() {
+    const [ano, setAno] = useState("");
+    const [feriados, setFeriados] = useState([]);
+
+    async function buscarFeriados() {
+        try {
+            const dataFeriados = await getConexaoapi(ano); // Chama a função para buscar os feriados
+            setFeriados(dataFeriados); 
+        } catch (error) {
+            console.error("Erro ao buscar feriados:", error);
+        }
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.formLabel}>Nome: {props.nome}</Text>
-            
-            <Text style={styles.formLabel}>Data: {props.data}</Text>
-            
+        //formulário com os botões
+        <View style={styles.formContext}>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Ano:</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="numeric"
+                    value={ano}
+                    onChangeText={setAno}
+                    placeholder="Digite o ano"
+                />
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={buscarFeriados}
+                >
+                    <Text style={styles.textButton}>Buscar Feriados</Text>
+                </TouchableOpacity>
+            </View>
+          
+            <View style={styles.resultContainer}>
+                <Text style={styles.resultLabel}>Feriados:</Text>
+                {feriados.length > 0 ? (
+                    feriados.map((feriado, index) => (
+                        <Result
+                            key={index}
+                            nome={feriado.name} 
+                            data={feriado.date} 
+                        
+                        />
+                    ))
+                ) : (
+                    <Text style={styles.formLabel}> Digite o ano desejado </Text>
+                )}
+            </View>
         </View>
     );
 }
