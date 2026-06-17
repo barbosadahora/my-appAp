1b79aca1b9635de60bd5ee8efa2c545de36949a0
barbosadahora <103425559+barbosadahora@users.noreply.github.com>
2026-06-16 23:43:27 -0300

    Refactor Form component and add Result function

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,44 +1,14 @@
-import React, { useEffect, useState} from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./style";
 
-import api from '../../services/api';
-import Result from './src/Result';
-
-export default function Form() {
-  const [filmes, setFilmes] = useState([]);
-  const [resultado, setResultado] = useState(null);
-  const [msg, setMsg] = useState(null);
-
-  useEffect(()=> {
-
-    async function loadFilmes(){
-      const response = await api.get('r-api/?api=filmes');
-      // console.log(response.data);
-      setFilmes(response.data);
-    }
-
-    loadFilmes();
-
-  }, []);
-  
-
- return (
-   <View style={styles.container}>
-     
-     <FlatList
-     data={filmes}
-     keyExtractor={ item => String(item.id) }
-     renderItem={ ({ item }) => <Filmes data={item} /> }
-     />
-
-   </View>
-  );
+export function Result(props) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.formLabel}>Nome: {props.nome}</Text>
+            
+            <Text style={styles.formLabel}>Data: {props.data}</Text>
+            
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-  }
-
-
-});
